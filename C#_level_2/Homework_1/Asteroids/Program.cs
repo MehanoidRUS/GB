86fe1a86138af7e5990b0d3c464b379591c9d424
Pavel Nefedov <pvmehanoid@gmail.com>
2018-02-14 19:28:41 +0300

    Проект Asteroids

---
+++ C#_level_2/Homework_1/Asteroids/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+namespace Asteroids
+{
+    class Program
+    {
+        static void Main()
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            form.Show();
+            Application.Run(form);
+        }
+        
+        
+        
+    }
+}
