f4eea3810a998142e0a11747eb660304b3671e38
Pavel Nefedov <pvmehanoid@gmail.com>
2018-02-21 16:12:34 +0300

    Добавлено: Изображение Ship и bullet_ship, добавлен класс Ship Изменено: Цикл создания свезд

---
+++ C#_level_2/Homework_1/Asteroids/Ship.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+
+namespace Asteroids
+{
+    /*Класс реализации персонажа*/
+    class Ship : BaseObject
+    {
+        //Колличесвто жизней
+        private int live = 3;
+        //Изображение персонажа
+        Bitmap image= new Bitmap(@"Image\ship.png");
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Ship()
+        {
+            this.pos = new Point(0,Game.Height/2);
+            speed = 1;
+        }
+        /// <summary>
+        /// Метод возвращает колличество жизней
+        /// </summary>
+        public int Live => live;
+        /// <summary>
+        /// Метод уменьшает кол-во жизней на 1
+        /// </summary>
+        public int Damage => live--;
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(image, pos.X, pos.Y);
+        }
+
+        /// <summary>
+        /// Перемещение объекта вверх
+        /// </summary>
+        public void Up()
+        {
+            pos.Y += speed;
+        }
+        /// <summary>
+        /// Перемещение объекта внизS
+        /// </summary>
+        public void Down()
+        {
+            pos.Y -= speed;
+        }
+
+        public override void ReCreation()
+        {
+        }
+
+        public override void Update()
+        {
+        }
+    }
+}
