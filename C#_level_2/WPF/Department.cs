9adc1a93a033b26778be83f67e5abdc7b4207a9a
Pavel Nefedov <pvmehanoid@gmail.com>
2018-03-01 17:27:03 +0300

    ДЗ по WPF

---
+++ C#_level_2/WPF/Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Department
+    {
+        string name;
+        public ObservableCollection<Employee> Employees;
+
+        /// <summary>
+        /// Создает объект департамент с указанным названием
+        /// </summary>
+        /// <param name="name">Название департамента</param>
+        public Department(string name)
+        {
+            this.name = name;
+            Employees = new ObservableCollection<Employee>();
+        }
+        public void AddEmployee(string name, string surname, int salary)
+        {
+            Employees.Add(new Employee(name, surname, salary));
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
---
+++ C#_level_2/WPF/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Salary { get; private set; }
+        
+        /// <summary>
+        /// Задает Имя Фамилию и зарплату сотрудника
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="surname">Фамилия</param>
+        /// <param name="salary">Зарплата</param>
+        public Employee(string name, string surname, int salary)
+        {
+            Name =name;
+            Surname = surname;
+            Salary = salary;
+        }
+
+
+        /// <summary>
+        /// Перегрузка метода ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"{Name} {Surname} {Salary}";
+        }
+    }
+}
